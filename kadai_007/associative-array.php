6fc71148b6b307919ac40a56f520338064c44938
1fuji2watori <xvghtvv@gmail.com>
2024-08-26 22:14:38 +0900

    Add files via upload

---
+++ kadai_007/associative-array.php
@@ -0,0 +1,15 @@
+<!DOCTYPE html>
+<html lan="ja">
+    <head>
+        <meta charset="utf-8">
+    </head>
+
+    <body>
+        <p>
+            <?php
+            $kadai_007 = ['name'=>'onion', 'price' => 200, 'weight' => 160];
+            print_r($kadai_007);
+            ?>
+        </p>
+    </body>
+</html>
